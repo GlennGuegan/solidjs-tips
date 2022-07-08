881ecf1b65b0e6f03d0638ec0d36cbdee64c67e5
Glenn Guegan <glenn.guegan@gmail.com>
2022-07-08 12:09:01 +0200

    🧑‍💻 add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import { Router } from "solid-app-router"
 import "./index.css"
 import App from "./App"
 
+import * as Sentry from "@sentry/browser"
+import { BrowserTracing } from "@sentry/tracing"
+
+Sentry.init({
+  dsn: "https://e8c670e555ba4b4cac899a41d233cf9e@o1310262.ingest.sentry.io/6557389",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
 render(
   () => (
     <Router>
